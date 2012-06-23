5f879f447bdcc550a50e84523f01fdc71427656b
Barrett Lewis <musikal.fusion@gmail.com>
2012-06-23 04:04:57 -0700

    Fixed errors with regex and error handling
    Forgot to create file on copy error, it should now minimize the ammount
    of testing that has to happen for folders to be created. Everything
    seems to work properly now.

--- Backup.py
+++ Backup.py
@@ -13,7 +13,7 @@ import shutil
 IMAGE_MATCH_STRING = "^((jpe?g)|(bmp)|(img)|(dds)|(dng)|(gif)|(png)|(psd)|(pspimage)|(tga)|(thm)|(tif)|(yuv))$"
 #This matches most valid months - DOES NOT acct for number of days in a month
 #only allows months 1-12, and dates 1
-DATE_MATCH = "^(1[012]|0[1-9])-([12]\d|3[01]|[1-9])-[12]\d{3}$"
+DATE_MATCH = "^(1[012]|0[1-9])-([12]\d|3[01]|0[1-9])-[12]\d{3}$"
 #get the users desktop
 #this is probably platform independant - as long as they have a desktop
 DESTINATION_PATH = os.path.join(os.path.expanduser('~'),'Desktop','My_Pics')
@@ -36,7 +36,11 @@ def Main():
         #originalFileName '_' The day abbreviation Mon-Day-Year (all as numbers) Hour;Minute;Second from a 24 hour clock - all times/dates are when file was created
         fileName = time.strftime("{0}_%a_%m-%d-%Y_%H;%M;%S.{1}".format(file, fileExtension), time.gmtime(os.path.getctime(filePath)))
         #this should be the date - allows more versitile user names for files that have underscores in them
-        folderName = next(section for section in str.split(fileName, '_') if re.match(DATE_MATCH, section))#get first match of the date value for folder name
+        folderName = None
+        try:
+          folderName = next(section for section in str.split(fileName, '_') if re.match(DATE_MATCH, section))#get first match of the date value for folder name
+        except:
+          folderName = "Undated"
         folderPath = os.path.join(DESTINATION_PATH, folderName)
         filePath = os.path.join(folderPath, fileName)
 
@@ -47,6 +51,7 @@ def Main():
           except IOError:#if there was an error 
             if not os.path.exists(folderPath):#the file wasn't there, make it and then redo the copy
               print("creating folder: " + folderName)
+              os.makedirs(folderPath)
               shutil.copy2(os.path.join(root, file), filePath)
         else:
           print ("Not backing up: " + file)
