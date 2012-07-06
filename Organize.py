c8c34c1c633793d3f232f36fddc8478a4a805d67
Barrett Lewis <musikal.fusion@gmail.com>
2012-07-06 03:42:27 -0700

    Finished work on the organizer
    I believe it works, moves don't overwrite old files. If a name conflict
    exists (ie a file was made on the same day with a name previously used)
    a number is appended.
    Thus if a file "A_picture.jpg" has been moved, and another
    "A_picture.jpg" needs to be moved, the second will be called
    "A_picture(1).jpg"
    This seems pretty standard for this sort of thing. It is now working.
    
    I couldn't figure out a ETAFTP solution for the move so It may have
    redundant calls to os.path.exists which is a bummer, but i can't think
    of a better way off the top of my head.

--- Organize.py
+++ Organize.py
@@ -13,7 +13,7 @@ import shutil
 IMAGE_MATCH_STRING = "^((jpe?g)|(bmp)|(img)|(dds)|(dng)|(gif)|(png)|(psd)|(pspimage)|(tga)|(thm)|(tif)|(yuv))$"
 #This matches most valid months - DOES NOT acct for number of days in a month
 #only allows months 1-12, and dates 1
-DATE_MATCH = "(\d\d-){2}\d\d\d\d"
+DATE_MATCH = "(\d\d-){2}(\d){4}"
 #get the users desktop
 #this is probably platform independant - as long as they have a desktop
 DESTINATION_PATH = os.path.join(os.getcwd(), "My Pictures")
@@ -27,14 +27,13 @@ def Main():
 
   #walk over the directories
   for root, dirs, files in os.walk(currentDirectory):
-    for file in files:
+    for file in files:#get all files in the current directory
       fileExtension = str.split(file, '.')[-1]
 
       if re.match(IMAGE_MATCH_STRING, fileExtension.lower()):
         filePath = os.path.join(root, file)
 
         if re.search(DATE_MATCH, str(filePath)):#if we are in a folder that is already organized skip
-          print("Not moving: " + file)
           continue
 
         #file name will have the following structure
@@ -42,14 +41,32 @@ def Main():
         folderName = time.strftime("%m-%d-%Y", time.gmtime(os.path.getctime(filePath)))
         folderPath = os.path.join(DESTINATION_PATH, folderName)
         newFilePath = os.path.join(folderPath, file)
+
+        if os.path.exists(newFilePath):#the picture already exists
+          modifier = 1
+          foundNumber = False
+          while not foundNumber: #we are going to modify the name so that it has a number in it to differentiate them, causing overwrites to not happen
+            fileParts = str.split(file, '.')
+            fileParts[-2] = fileParts[-2] + '(' + str(modifier) + ')'
+            possibleFilePath = os.path.join(folderPath, '.'.join(fileParts))
+            if not os.path.exists(possibleFilePath):
+              foundNumber = True
+              newFilePath = possibleFilePath
+            else:
+              modifier += 1
         
         print ("Moving: " + file)
         try:#attempt to move the file
           shutil.move(filePath, newFilePath)
         except:#if there was an error 
+          doCopy = False
+
           if not os.path.exists(folderPath):#the file wasn't there, make it and then redo the move
             print("creating folder: " + folderName)
             os.makedirs(folderPath)
+            doCopy = True
+            
+          if doCopy:
             shutil.move(filePath, newFilePath)
 
 #run program
